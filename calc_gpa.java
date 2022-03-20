26c422b008ef22ceca4ff586c3b0cf454881e1c3
esraa4037 <71180605+esraa4037@users.noreply.github.com>
2022-03-20 21:55:45 +0200

    Update calc_gpa.java

--- calc_gpa.java
+++ calc_gpa.java
@@ -1,137 +1,148 @@
-package swTesting;
-public class asss
+import java.util.Scanner;
+
+public class Main
 {
-    private int id;
-    private String name;
-    private int marks[];
-    private float average;
-    private String grade;
-    public asss(int a,String b,int[] c)
-    {
-        id=a;
-        name=b;
-        marks=c;
-    }
-    public void setId(int n)
-    {
-        id=n;
-    }
-    public int getId()
-    {
-        return id;
-    }
-    public void setMarks(int[] marks)
-    {
-        this.marks=marks;
-    }
-    public int[] getMarks()
-    {
-        return marks;
-    }
-    public void setName(String n)
-    {
-        name=n;
+    public static void main(String args[]){
+        int[] marks = {90, 90, 80, 70, 40, 80};
+        float[] gpas = conver_to_gpa(marks);
         
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public void setAverage(float n)
-    {
-        average=n;
-    }
-    public float getAverage()
-    {
-        return average;
-    }
-    public void setGrade(String n)
-    {
-        grade=n;
-    }
-    public String getGrade()
-    {
-        return grade;
-    }
-    public void calculateAvg()
-    {
-        float a1=0.0F;
-        for(int a=0;a<this.getMarks().length;a++)
-        {
-            a1=a1+this.marks[a];
-        }
-        this.setAverage(a1/getMarks().length);
-    }
-    public void findGrade()
-    {
-        int min=this.marks[0];
-       for(int b=0;b<this.getMarks().length;b++)
-        {
-            if(this.marks[b]<min) 
-                {
-                    min=this.marks[b];
-                }
-        }
-        if(min<60)
-        {
-            this.setGrade("F");
-            System.out.println("F");
-        }
-        else if(this.getAverage()>=97 && this.getAverage()<=100)
-        {
-            this.setGrade("A+");
-            System.out.println("A+");
-        }
-        else if(this.getAverage()>=93 && this.getAverage()<97)
-        {
-        	this.setGrade("A");
-        	System.out.println("A");
-        }
-        else if(this.getAverage()>=90 && this.getAverage()<93)
-        {
-        	this.setGrade("A-");
-        	System.out.println("A-");
-        }
-        else if(this.getAverage()>=87 && this.getAverage()<90)
-        {
-        	this.setGrade("B+");
-        	System.out.println("B+");
-        }
-        else if(this.getAverage()>=83 && this.getAverage()<87)
-        {
-        	this.setGrade("B");
-        	System.out.println("B");
-        }
-        else if(this.getAverage()>=80 && this.getAverage()<83)
-        {
-        	this.setGrade("B-");
-        	System.out.println("B-");
-        }
-        else if(this.getAverage()>=77 && this.getAverage()<80)
-        {
-        	this.setGrade("C+");
-        	System.out.println("C+");
-        }
-        else if(this.getAverage()>=73 && this.getAverage()<77)
-        {
-        	this.setGrade("C");
-        	System.out.println("C");
-        }
-        else if(this.getAverage()>=70 && this.getAverage()<73)
-        {
-        	this.setGrade("C-");
-        	System.out.println("C-");
-        }
-        else if(this.getAverage()>=67 && this.getAverage()<70)
-        {
-        	this.setGrade("D+");
-        	System.out.println("D+");
-        }
-        else if(this.getAverage()>=60 && this.getAverage()<67)
-        {
-        	this.setGrade("D");
-        	System.out.println("D");
+        int[] weights = {4, 3,3,2,3, 4};
+        double gpa = calc_gpa(marks, weights);
+        System.out.println(gpa);
+        System.out.println(getRank(gpa));
+        for (int i = 0; i < gpas.length; i++){
+            System.out.print(gpas[i] + " ");
         }
         
     }
+	public static void takeInput(int[] marks, int[] weights) {
+		
+		Scanner scanner = new Scanner(System.in);
+		for(int i = 0; i < 6; i++) {
+			marks[i] = scanner.nextInt();
+			weights[i] = scanner.nextInt();
+			}
+		scanner.close();
+	}
+	
+	public static double calc_gpa (int[] marks, int[] weights){
+		int den = 0, num = 0;
+		for(int i = 0; i < marks.length; i++) {
+			den += weights[i];
+		}
+		for(int i = 0; i < marks.length; i++) {
+			num += marks[i]*weights[i];
+		}
+		double gpa = num/(double)den;
+		return gpa;
+	}
+	
+	
+	
+	public static String getRank(double gpa) {
+		if(gpa<60)
+        {
+            return "F";
+        }
+        else if(gpa>=97 && gpa<=100)
+        {
+            return "A+";
+        }
+        else if(gpa>=93 && gpa<97)
+        {
+        	return "A";
+        }
+        else if(gpa>=90 && gpa<93)
+        {
+        	return "A-";
+        }
+        else if(gpa>=87 && gpa<90)
+        {
+        	return "B+";
+        }
+        else if(gpa>=83 && gpa<87)
+        {
+        	return"B";
+        }
+        else if(gpa>=80 && gpa<83)
+        {
+         	return "B-";
+        }
+        else if(gpa>=77 && gpa<80)
+        {
+        	return "C+";
+        }
+        else if(gpa>=73 && gpa<77)
+        {
+        	return "C";
+        }
+        else if(gpa>=70 && gpa<73)
+        {
+        	return "C-";
+        }
+        else if(gpa>=67 && gpa<70)
+        {
+        	return "D+";
+        }
+        else if(gpa>=60 && gpa<67)
+        {
+        	return "D";
+        }
+        return "unknown";
+    }
+	
+		
+	public static float[] conver_to_gpa(int[] marks) {
+		float gpa[] = new float[6];
+		for(int i = 0; i < marks.length; i++){
+		    
+		if(marks[i]<60)
+        {
+            gpa[i] = 0.0f;
+        }
+        else if(marks[i]>=94 && marks[i]<=100)
+        {
+            gpa[i] = 4f;
+        }
+        
+        else if(marks[i]>=90 && marks[i]<94)
+        {
+        	gpa[i] = 3.7f;
+        }
+        else if(marks[i]>=87 && marks[i]<90)
+        {
+        	gpa[i] = 3.3f;
+        }
+        else if(marks[i]>=83 && marks[i]<87)
+        {
+         	gpa[i] = 3f;
+        }
+        else if(marks[i]>=80 && marks[i]<83)
+        {
+        	gpa[i] = 2.7f;
+        }
+        else if(marks[i]>=77 && marks[i]<80)
+        {
+        	gpa[i] = 2.3f;
+        }
+        else if(marks[i]>=73 && marks[i]<77)
+        {
+        	gpa[i] = 2f;
+        }
+        else if(marks[i]>=70 && marks[i]<73)
+        {
+        	gpa[i] = 1.7f;
+        }
+        else if(marks[i]>=67 && marks[i]<70)
+        {
+        	gpa[i] = 1.3f;
+        }
+        else if(marks[i]>=60 && marks[i]<67)
+        {
+        	gpa[i] = 1.0f;
+        }
+    }
+    return gpa;
+}
+	       
 }
